495bdc0f1b937e53e4b06eb248061a1afcdae946
Mark Struberg <struberg@apache.org>
2016-07-15 23:18:08 +0200

    more JavaDoc

--- api/src/main/java/javx/config/Config.java
+++ api/src/main/java/javx/config/Config.java
@@ -24,8 +24,9 @@ import javx.config.spi.ConfigSource;
 import javx.config.spi.ConfigSourceProvider;
 
 /**
- * <p>Resolves configured values of properties by going through the list of configured {@link ConfigSource}s and using the
- * one with the highest ordinal. If multiple {@link ConfigSource}s have the same ordinal, their order is undefined.</p>
+ * <p>Resolves configured values of properties by going through the list
+ * of configured {@link ConfigSource}s and using the one with the highest ordinal.
+ * If multiple {@link ConfigSource}s have the same ordinal, their order is undefined.</p>
  *
  * <p>You can provide your own lookup paths by implementing and registering additional
  * {@link ConfigSource}s and {@link ConfigSourceProvider} implementations.</p>
@@ -34,17 +35,58 @@ import javx.config.spi.ConfigSourceProvider;
  */
 public interface Config {
 
+    /**
+     * Resolves the value configured for the given key.
+     *
+     * @param key the property key
+     *
+     * @return the configured property value from the {@link ConfigSource} with the highest ordinal or null if there is
+     *         no configured value for it
+     */
     String getPropertyValue(String key);
 
+    /**
+     * Returns a Map of all properties from all scannable config sources. The values of the properties reflect the
+     * values that would be obtained by a call to {@link #getPropertyValue(java.lang.String)}, that is, the value of the
+     * property from the ConfigSource with the highest ordinal.
+     *
+     * @see ConfigSource#isScannable()
+     */
     Map<String, String> getAllProperties();
 
+    /**
+     * Filter the configured value.
+     * This can e.g. be used for decryption.
+     * @return the filtered value
+     */
     String filterConfigValue(String key, String value);
 
+    /**
+     * Filter the configured value for logging.
+     * This can e.g. be used for displaying ***** instead of a real password.
+     * @return the filtered value
+     */
     String filterConfigValueForLog(String key, String value);
 
+    /**
+     * @return all currently registered {@link ConfigSource}s
+     */
     ConfigSource[] getConfigSources();
 
-    void addConfigSources(List<ConfigSource> configSources);
 
-    void addConfigFilter(ConfigFilter configFilter);
+    /**
+     * This method can be used for programmatically adding {@link ConfigSource}s
+     * to this very Config.
+     * It is not needed for normal 'usage' by end users, but only for Extension Developers!
+     *
+     * @param configSourcesToAdd the ConfigSources to add
+     */
+    void addConfigSources(List<ConfigSource> configSourcesToAdd);
+
+    /**
+     * Add a {@link ConfigFilter} to this very Config
+     *
+     * It is not needed for normal 'usage' by end users, but only for Extension Developers!
+     */
+    void addConfigFilter(ConfigFilter configFilterToAdd);
 }
