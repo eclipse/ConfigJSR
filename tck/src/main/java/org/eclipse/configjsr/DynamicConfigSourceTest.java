bf22bdf7af6b084f82d13b3644e62de96bf8b02f
daniel-dos <daniel.dias.analistati@gmail.com>
2018-07-31 00:48:29 -0300

    fix-test tck-build.
    Signed-off-by: daniel-dos <daniel.dias.analistati@gmail.com>

--- tck/src/main/java/org/eclipse/configjsr/DynamicConfigSourceTest.java
+++ tck/src/main/java/org/eclipse/configjsr/DynamicConfigSourceTest.java
@@ -35,7 +35,7 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
